518cf1068fa972be3e1dd8f9dfbb7da0d20107a3
Christophe Quignon <cuignon@gmail.com>
2015-04-25 10:38:46 +0200

    1+1 added, increasing track size missing

--- exercise_4/exercise_4_EA.py
+++ exercise_4/exercise_4_EA.py
@@ -9,14 +9,15 @@ from multiprocessing import Pool
 import matplotlib.pyplot as plt
 import csv
 
-import fitness as ft
+import vehicle_simulation as ft
+
+import copy
 
 
 RANGE = ft.dist.shape[0]
 
 class Genome():
     def __init__(self, genotype):
-        #self.mutation_prob = 1.0 #TODO: check if this is good
         self.genotype = []
         self.set_genotype(genotype)
 
@@ -25,54 +26,33 @@ class Genome():
             trackpoints, motor_values = zip(*genotype)
         else:
             #empty genotype
-            self.genotype = [(0.0, 0.0)]
+            print "empty genome"
+            self.genotype = [(0.0, 1.0)]
             return
 
         if len(trackpoints) != len(motor_values):
             print "Error in genotype"
         elif any(t > RANGE or t < 0 for t in trackpoints):
-            print 'Error in trackpoints'
+            print 'Error in trackpoints: ', trackpoints
         elif any(m < 0.0 or m > 1.0 for m in motor_values):
-            print 'error with motor values'
+            print 'error with motor values: ', motor_values
         else:
             self.genotype = genotype
 
 
 
-    def mutate(self):
-        tps = [x[0] for x in self.genotype]
+    def mutate(self, sigma):
 
-        if tps[0] != 0:
-            tps.insert(0, 0)
-        if tps[-1] != RANGE:
-            tps.append(RANGE)
+        #% MAY BE BAD ( uniform distribution, you know...)
 
-        new_tsps = []
-        for i in range(1, len(tps)-1):
-            #sigma is minium distance to neighbor
-            #mu is own position
-            mu = tps[i]
-            sigma = min([tps[i] - tps[i-1], tps[i+1] - tps[i]])
-            new_tsps.append(gauss(mu, sigma))
+        new_tps = [gauss(mu[0], sigma)%RANGE for mu in self.genotype]
 
-        new_mvs = [gauss(x[1], 0.5)%1 for x in self.genotype]
-        # print new_mvs
-        # print new_tsps
-        self.set_genotype(zip(new_tsps, new_mvs))
+        #the motor command sigma naturally 0.5
+        new_mvs = [gauss(x[1], 0.5)%1 for x in self.genotype]#%1
+        # print "mutated:"
+        # print new_tps, new_mvs
+        self.set_genotype(zip(new_tps, new_mvs))
 
-        # #TODO:  Zero mean Gaussian
-        # if random() < self.mutation_prob:
-        #
-        #     tp = randint(0, RANGE-1)
-        #     mv = random()
-        #
-        #     mutation_idx = randint(0, len(self.genotype)-1)
-        #
-        #     print 'mutating idx' + str(mutation_idx)
-        #
-        #     mutation = self.genotype
-        #     mutation[mutation_idx] = (tp, mv)
-    #         self.set_genotype(mutation)
 
     # def breed(self, partner):
     #     basis_tp, basis_mv = zip(*self.genotype)
@@ -86,7 +66,7 @@ class Genome():
     #
     #     return Genome(self.mutation_prob, kid_genotype)
 
-    def get_run(self):
+    def fitness(self):
         gt_sort = sorted(self.genotype, key = lambda x : x[0])
         run = []
         last_mv = 0.0#TODO: Check whether thats a good idea...
@@ -96,9 +76,12 @@ class Genome():
                 _, last_mv  = gt_sort.pop(0)
             else:
                 pass #we just append
-
             run.append(last_mv)
-        return np.asarray(run)
+        try:
+            fit =  ft.vehicle_fitness(np.asarray(run))[0]
+        except SystemExit:
+            return float('inf')
+        return fit
 
 class Evolution():
     def __init__(self, iterations, pop_size, selection_type):
@@ -107,64 +90,117 @@ class Evolution():
         self.selection_type = selection_type
 
         self.pop = []
+        self.sigma = 0.0
+
+        self.sigma_increase = 0.0#0.0 means disabled
+        self.mutations = 1
+        self.improvements = 1
 
         self.mins = []
         self.maxs = []
         self.means = []
 
         ##INITIALIZATION
-        for _ in range(self.pop_size):
-            steps = randint(0, RANGE)/1000#TODO scale
-            genotype = []
-            for _ in range(steps):
-                sp = randint(0, RANGE)
-                mv = random()
-                genotype.append((sp, mv))
-            self.pop.append(Genome(genotype))
+        for _ in range(pop_size):
+            # g = Genome([(0, 1.0)])# we start with full power
+            g = Genome([(0, 0.0)])# we start with nothing
+
+            self.pop.append(g)
+
+            while g.fitness() < 0:
+                steps = randint(0, 10)#%10#TODO scale
+                genotype = []
+                for _ in range(steps):
+                    sp = randint(0, RANGE)
+                    mv = random()
+                    genotype.append((sp, mv))
+                g = Genome(genotype)
+                # print genotype
+
+            self.pop.append(g)
+        print "initialisation done"
 
 
     def run(self):
         for i in range(self.iterations):
             #no more crossover
             self.evaluation()
-            # self.recombination()
-            #self.selection()
-            self.mutation()
+            self.recombination()
+            self.selection()
+            # self.mutation()
+            # mutation happens inside selection
+            # print "select:"
+            print map(lambda x: x.fitness(), self.pop)
 
-            # print map(lambda x : x.genotype, self.pop)
-            print map(lambda x: ft.vehicle_fitness(x.get_run())[0], self.pop)
+        print "DONE"
 
         return# self.mins, self.means, self.maxs, self.best_genotype
 
     def selection(self):
         #environmental selection
-        #ELIF selction_type
         #(1+1), (mu, lambda) and (mu + lambda)
+        if '1' in self.selection_type:
+            # print 'one on one!'
+            parent = self.pop[0]
+            kid = copy.deepcopy(self.pop[0])
+            kid.mutate(self.sigma)
+            self.pop = [parent, kid]
 
-        pass
+            self.mutations = self.mutations + 1
+
+            #TODO
+        elif '+' in self.selection_type:
+            print 'plus!'
+            #TODO
+        elif ',' in self.selection_type.contains:
+            print 'comma'
+            #TODO
+        # print "selection done"
 
     def recombination(self):
-        #TODO 1/5th rule
 
-        #improvement/mutation > 1/5.0: simga --
-        #improvement/mutation < 1/5.0: simga ++
+        #TODO 1/5th rule
+        if self.sigma_increase != 0.0:
+            imp_rate = self.improvements/float(self.mutations)
+
+            if imp_rate > 1/5.0:
+                # simga --
+                self.sigma = self.sigma - self.sigma_increase
+                print "decreased to ", self.sigma
+            if imp_rate < 1/5.0:
+                #simga ++
+                self.sigma = self.sigma + self.sigma_increase
+                print "increased to ", self.sigma
+        else:
+            #silently skip
+            pass
         pass
 
-    def mutation(self):
-        #zero mean gaussian mutation on all
-        # print 'mutate'
-        #mutate all the genomes!
-        for g in self.pop:
-            g.mutate()
+    # def mutation(self):
+    #     #mutate all the genomes!
+    #     for g in self.pop:
+    #         g.mutate()
 
     def evaluation(self):
-        self.pop = sorted(self.pop, key = lambda g : ft.vehicle_fitness(g.get_run())[0])
-        pass
+
+        #efficient sorting and know whether the min ha schanged
+
+        #we sort the list, but only safe the new keys
+        #equivalent to argsort() in numpy
+        sort_keys = sorted(range(len(self.pop)), key=lambda k: self.pop[k].fitness())
+
+        #if elemt changed...
+        if sort_keys[0] != 0:
+            print "improved!"
+            self.improvements = self.improvements + 1
+
+        #we now sort by the index
+        self.pop = [self.pop[i] for i in sort_keys]
 
 
 
 ea =  Evolution(
-                iterations = 1,
-                pop_size = 4,
-                selection_type = '+')
+                iterations = 100,
+                pop_size = 1,
+                selection_type = '1+1')
 ea.run()
