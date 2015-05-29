5bc6f2cc443e6620d8e39c25770740074cbb370d
Christophe Quignon <christophe.quignon@recogizer.de>
2015-05-29 14:28:20 +0200

    genetics including crossover added

--- project/src/decisiontree.py
+++ project/src/decisiontree.py
@@ -4,21 +4,28 @@ import numpy as np
 import pylab
 import pandas as pd
 import matplotlib.pyplot as plt
+from imp import load_source
 
-# # TESTING
-# df = pd.read_csv('data/ds1_weather.csv', decimal=',',sep=';', parse_dates=True, index_col=[0])
-#
-# df = df.resample('1Min')
-# df.interpolate(inplace=True)
-# df.fillna(inplace=True, method='ffill')#we at first forwardfill
-# df.fillna(inplace=True, method='bfill')#then do a backwards fill
+# TESTING
+df = pd.read_csv('data/ds1_weather.csv', decimal=',',sep=';', parse_dates=True, index_col=[0])
+
+df = df.resample('1Min')
+df.interpolate(inplace=True)
+df.fillna(inplace=True, method='ffill')#we at first forwardfill
+df.fillna(inplace=True, method='bfill')#then do a backwards fill
 
 
-class decisionLeaf():
+class DecisionLeaf(object):
 
   def __init__(self, val):
     self.val = val
     self.mse = float('inf')
+
+  def __iter__(self):
+    yield self
+
+  def __repr__(self):
+      return str(self.val)
 
   def predict(self, predictionset):
     return [self.val for _ in predictionset.index]
@@ -32,8 +39,13 @@ class decisionLeaf():
     self.mse = ((result - output) ** 2).mean()
     return self.mse
 
+  def depth(self):
+    return 1
 
-class decisionTree():
+  def size(self):
+    return 1
+
+class DecisionTree(object):
 
   def __init__(self, feature, split, left_child, right_child):
     self.feature = feature
@@ -47,15 +59,60 @@ class decisionTree():
     self.left_child = []
     self.set_left_child(left_child)
 
+  def __iter__(self):
+    yield self
+    for left in self.left_child:
+        yield left
+    for right in self.right_child:
+        yield right
+
+
+  def __getitem__(self, key):
+    return [x for x in self][key]
+
+  def __setitem__(self, key, item):
+    left_items = self.left_child.size()
+
+    if key == 0:
+        print "CANNOT CHANGE ITSELF"
+        return
+
+    if key == 1:
+        self.left_child = item
+        return
+
+    if key == left_items + 1:
+        self.right_child = item
+        return
+
+    if key <= left_items:
+        self.left_child[key - 1] = item
+        return
+
+    if key > left_items + 1:
+        self.right_child[key - left_items-1] = item
+        return
+
+  def __repr__(self):
+      s = 'if x[' + str(self.feature) + '] < ' + str(self.split) + ':\n'
+
+      for left_line in str(self.left_child).split('\n'):#indent
+          s = s + '  ' + left_line + '\n'
+      s = s + 'else:\n'
+
+      for right_line in str(self.right_child).split('\n'):#indent
+          s = s + '  ' + right_line + '\n'
+      return s
+
   def set_left_child(self, left_child):
     if isinstance(left_child, float):
-      self.left_child = decisionLeaf(left_child)
+      self.left_child = DecisionLeaf(left_child)
     else:
       self.left_child = left_child
 
   def set_right_child(self, right_child):
     if isinstance(right_child, float):
-      self.right_child = decisionLeaf(right_child)
+      self.right_child = DecisionLeaf(right_child)
     else:
       self.right_child = right_child
 
@@ -112,11 +169,24 @@ class decisionTree():
 
     return result
 
+  def depth(self):
+    left_depth = self.left_child.depth()
+    right_depth = self.right_child.depth()
+    return 1 + (max(left_depth, right_depth))
+
+  def size(self):
+    left_size = self.left_child.size()
+    right_size = self.right_child.size()
+    return 1 + left_size + right_size
+
 
 #
 # #TESTING
 #
-# dT = decisionTree('Aussentemperatur', 7, 12.5, 56.5)
-# dN = decisionTree('Vorlauftemperatur', 13, dT, 57.0)
-# dN.update_mse(df[0:100], df.Energie[0:100])
-# dN.predict(df[10:20])
+dT = DecisionTree('Aussentemperatur', 7, 7.7, 64.0)
+dN = DecisionTree('Vorlauftemperatur', 8.2, 0.2, 64.6)
+dN[1] = dT
+dN.update_mse(df[0:100], df.Energie[0:100])
+print dN.predict(df[10:20])
+
+print dN
--- project/src/genetics.py
+++ project/src/genetics.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 from imp import load_source
-from random import randrange, random
+from random import randrange, random, choice
 import numpy as np
 import pylab
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import tree
 from decisiontree import *
+from copy import deepcopy
 
 #import dataset
 #equivalent to:
@@ -23,7 +24,83 @@ df.fillna(inplace=True, method='ffill')#we at first forwardfill
 df.fillna(inplace=True, method='bfill')#then do a backwards fill
 
 
-dT = decisionTree('Aussentemperatur', 7, 7.7, 64.0)
-dN = decisionTree('Vorlauftemperatur', 8.2, dT, 64.6)
-dN.update_mse(df[0:100], df.Energie[0:100])
+class Genome():
+    def __init__(self, data, predict_feat, genotype, leaf_mutation = 0.1, node_mutation = 0.8):
+        if predict_feat in data.columns:
+            self.df = data
+            self.predict_feat = predict_feat
+        else:
+            print 'Feature ', predict_feat, ' could not be found.'
+            self.predict_feat = data.columns[0]
+            print 'Feature ', self.predict_feat, ' choosen instead.'
+
+        if isinstance(genotype, DecisionTree):
+            self.genotype = genotype
+        else:
+            print "Genotype ", genotype, 'unknown'
+            print "Random leaf set"
+
+            feature  = choice(self.df.columns)
+            self.genotype = DecisionLeaf(0.0)
+
+        self.leaf_mutation = leaf_mutation
+        self.node_mutation  = node_mutation
+
+
+    def mutate(self):
+        for subtree in self.genotype:
+
+            if isinstance(subtree, DecisionLeaf):
+                if random() < self.leaf_mutation:
+                    #TODO: mutate leaf
+                    pass
+            elif isinstance(subtree, DecisionTree):
+                if random() < self.node_mutation:
+                    #TODO: mutate node
+                    pass
+
+
+    def fitness(self):
+        pass
+
+    def crossover(self, partner):
+
+        #TODO balance randomnessbetween nodes and leafs
+
+        # we take a random position
+        own_idx = randrange(self.genotype.size())
+
+        # we take a random subtree
+        partner_idx = randrange(1, partner.genotype.size())
+        partner_subtree = partner.genotype[partner_idx]
+
+
+        if own_idx == 0:
+            self.genotype = deepcopy(partner_subtree)
+        else:
+            self.genotype[own_idx] = deepcopy(partner_subtree)
+
+
+
+dT = DecisionTree('Aussentemperatur', 7, 7.7, 64.0)
+dN1 = DecisionTree('Vorlauftemperatur', 8.2, dT, 64.6)
+dN1.update_mse(df[0:100], df.Energie[0:100])
 print dN.predict(df[10:20])
+
+dT = DecisionTree('Aussentemperatur', 17, 17.7, 164.0)
+dN2 = DecisionTree('Vorlauftemperatur', 18.2, dT, 164.6)
+dN2.update_mse(df[0:100], df.Energie[0:100])
+
+g1 = Genome(df, 'Energie', dN1)
+
+g2 = Genome(df, 'Energie', dN2)
+
+print g1.genotype
+print '__'
+print g2.genotype
+print 'CROSSOVER'
+
+
+g2.crossover(g1)
+
+print g2.genotype
