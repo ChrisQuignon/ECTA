7014ca90ab6d1fd7a3916d2c96bd167e79ad1965
Christophe Quignon <christophe.quignon@recogizer.de>
2015-06-24 15:41:17 +0200

    1k runs are enough

--- project/src/analysis.py
+++ project/src/analysis.py
@@ -3,44 +3,74 @@ from matplotlib import pylab
 import numpy as np
 
 
-mutate_run = pd.DataFrame.from_csv('img/overnight/mutation_run.csv', sep = ';', index_col=None)
+# mutate_run = pd.DataFrame.from_csv('img/last_runs/mutation_run.csv', sep = ';', index_col=None)
+# selection_run = pd.DataFrame.from_csv('img/last_runs/select_run.csv', sep = ';', index_col=None)
+#
+# all_runs = mutate_run.append(selection_run)
+all_runs = pd.DataFrame.from_csv('img/statsrun.csv', sep = ',', index_col=None)
 
-mutate_run=mutate_run.rename(columns = {'min':'Minimal MSE',
+all_runs=all_runs.rename(columns = {'min':'Minimal MSE',
                                   'leaf_mutation' : 'Leaf mutation rate',
                                   'node_mutation' : 'Node mutation rate',
                                   'predict_mse' : 'Prediction MSE',
+                                  "sigma" : 'Sigma',
+                                  "iterations" : 'Iterations',
+                                  "selection" : 'Selection Type',
+                                  "n_samples" : "MSE Samples"
                                  })
 
 
-relevant_values = ['Minimal MSE', 'Leaf mutation rate', 'Node mutation rate','Prediction MSE',]
+#parse selection
+slist = list(set(all_runs['Selection Type'].values))
+sindexes = [slist.index(stype) for stype in all_runs['Selection Type'].values]
+all_runs['Selection Type'] = sindexes
 
-#MINIMAL FITNESS
-m_min = mutate_run.sort(columns = ['Minimal MSE'], ascending=False)
+relevant_values = np.asarray(['Minimal MSE', 'Leaf mutation rate', 'Node mutation rate','Prediction MSE', "Sigma", "Iterations", "Selection Type", "MSE Samples"])
+
+
+#PREDICTED FITNESS
+all_runs = all_runs.sort(columns = ['Prediction MSE'], ascending=False)
 for idx in relevant_values:
-    pylab.plot(m_min[idx].values  / m_min[idx].max(), linestyle='.', marker = '.',  label= idx)
-    pylab.plot(m_min['Minimal MSE'].values / m_min['Minimal MSE'].max(), label='Minimal MSE')
-    pylab.xlabel('Run index, sorted by minimal MSE (fitness)')
-    pylab.ylabel('Normalized values')
-    pylab.ylim(0, 1.1)
+    pylab.plot(all_runs[idx].values, linestyle='.', marker = '.',  label= idx)
+    pylab.plot(all_runs['Prediction MSE'].values * float(all_runs[idx].max()*4), label='Scaled predicted MSE (fitness)')
+    pylab.xlabel('Run index, sorted by predicted MSE (fitness)')
+    pylab.ylabel(idx)
+    axes = pylab.gca()
+    ymin, ymax = axes.get_ylim()
+    pylab.ylim(ymin*1.1, ymax*1.3)
+
+    if idx in ["Selection Type"]:
+    # pylab.ylim(0, 1.1)
+        pylab.yticks(np.arange(0.0,4.0), slist)
+        pylab.ylim(-.2, ymax*1.3)
+
     pylab.legend()
     pylab.show()
 
 
-#PREDICTED FITNESS
-m_pred = mutate_run.sort(columns = ['Prediction MSE'], ascending=False)
+#PREDICTED MSE
+all_runs = all_runs.sort(columns = ['Minimal MSE'], ascending=False)
 for idx in relevant_values:
-    pylab.plot(m_pred[idx].values  / m_pred[idx].max(), linestyle='.', marker = '.',  label= idx)
-    pylab.plot(m_pred['Prediction MSE'].values / m_pred['Prediction MSE'].max(), label='Predicted MSE (fitness)')
-    pylab.xlabel('Run index, sorted by predicted MSE (fitness)')
-    pylab.ylabel('Normalized values')
-    pylab.ylim(0, 1.1)
+    pylab.plot(all_runs[idx].values, linestyle='.', marker = '.',  label= idx)
+    pylab.plot(all_runs['Minimal MSE'].values * float(all_runs[idx].max()*4), label='Scaled minimal MSE (fitness)')
+    pylab.xlabel('Run index, sorted by minimal MSE (fitness)')
+    pylab.ylabel(idx)
+    axes = pylab.gca()
+    ymin, ymax = axes.get_ylim()
+    pylab.ylim(ymin*1.1, ymax*1.3)
+
+    if idx in ["Selection Type"]:
+    # pylab.ylim(0, 1.1)
+        pylab.yticks(np.arange(0.0,4.0), slist)
+        pylab.ylim(-.2, ymax*1.3)
+
     pylab.legend()
     pylab.show()
 
 
 #SORTED EVERYTHING
 for idx in relevant_values:
-    s = m_pred.sort(columns = [idx], ascending=False)
+    s = all_runs.sort(columns = [idx], ascending=False)
     rest = [x for x in relevant_values]# if x != idx]
     for jdx in rest:
         pylab.plot((s[jdx] / s[jdx].max()).values, linestyle='.', marker = '.',  label=jdx)
@@ -53,15 +83,22 @@ for idx in relevant_values:
 
 
 #CORRELATION
-cm = mutate_run[relevant_values].corr()
+cm = all_runs[relevant_values].corr()
 cm = cm.as_matrix()
+
+# #REORDER
+sortkey = np.asarray([0, 3, 2, 7, 1, 4, 5, 6])
+cm = cm[sortkey]
+for i,  row in enumerate(cm):
+    cm[i] = row[sortkey]
+
 pylab.pcolor(cm, cmap='RdBu')
 cbar = pylab.colorbar()
 pylab.clim(-1,1)
 
-lable = map (lambda x: x.replace(' ', '\n'), relevant_values)
-pylab.yticks(np.arange(0.5,4.5),lable, rotation='horizontal')
-pylab.xticks(np.arange(0.5,4.5),lable, rotation='horizontal')
+lable = map (lambda x: x.replace(' ', '\n'), relevant_values[sortkey])
+pylab.yticks(np.arange(0.5,8.5),lable, rotation='horizontal')
+pylab.xticks(np.arange(0.5,8.5),lable, rotation='horizontal')
 # savefig('../img/correlation.png')
 
 pylab.title("Corellation matrix")
